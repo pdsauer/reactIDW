d12de25f0c9400171cff09d2d8ba6f168b010e44
Philipp Sauer <pdsauer@live.com>
2019-11-10 10:04:33 +0100

    fixed api

--- CineTicket/public/api.php
+++ CineTicket/public/api.php
@@ -1,28 +1,46 @@
 <?php
 
 header('Content-Type: application/json');
-$array = [[        
-            'name' => 'Joker 3d',
-            'date' => '21.11.2019',
-            'cinema' => 'Cinema 5',
-            'price' => 9.80
-        ],
-        [
-            'name' => 'Avengers Endgame',
-            'date' => '20.11.2019',
-            'cinema' => 'Cinema 7',
-            'price' => 7.60
-        ],
-        [
-            'name' => 'Forrest Gump',
-            'date' => '13.11.2019',
-            'cinema' => 'Cinema 6',
-            'price' => 5.60
-        ]];
 
-        $response = array(
-            'status' => true,
-            'data'=> $array
-        );
+
+
+if($_GET['i'] == 'movies'){
+    $array = [
+    [
+        'id' => 1,
+        'name' => 'Joker 3D',
+    ],[
+        'id' => 2,
+        'name' => 'Avengers Endgame',
+    ],[
+        'id' => 3,
+        'name' => 'Forrest Gump',
+    ]];
+} else if ($_GET['i'] == 'date'){
+    $array = [
+        [
+            'id' => 1,
+            'date' => '01.12.2019',
+        ],[
+            'id' => 2,
+            'date' => '02.12.2019',
+        ],[
+            'id' => 3,
+            'date' => '03.12.2019',
+        ]];
+} else if ($_GET['i'] == 'price'){
+    $array = 
+        [
+            'id' => 1,
+            'price' => 9.80,
+        ];
+} else {
+    $array = ['There was an error'];
+}
+
+    $response = array(
+        'status' => true,
+        'data'=> $array
+    );
 
 echo json_encode($response);
